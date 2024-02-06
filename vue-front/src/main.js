ecb2e51e494ea3e2fc9145caf1dc02e9efdfd226
hmydt <woal136@naver.com>
2024-02-06 18:14:31 +0900

    board list view template

--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// css는 끝에 붙일 것
+import './assets/common.css'
+
 createApp(App)
     .use(router)
     .mount('#app')
--- vue-front/src/router/index.js
+++ vue-front/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import PageHome from "@/views/PageHome";
+import BoardList from "@/views/board/BoardList"
 
 const routes = [
     {
         path: '/',
         name: 'PageHome',
         component: PageHome
+    },
+    {
+        path: '/board/list',
+        name: 'BoardList',
+        component: BoardList
     }
 ]
 
